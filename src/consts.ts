c2fe3a078c7143d350b87f2c1bd9e22c3fcca95a
raulsperoni <rsperoni@expertinstitute.com>
2025-12-11 13:59:34 -0300

    Remove migration and mobile setup guides, update site title, and enhance styling across components for improved aesthetics and usability.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Raúl Speroni';
+export const SITE_TITLE = 'Lo de Raúl';
 export const SITE_DESCRIPTION = 'Un baúl, un rincón, otra versión, yo.';
